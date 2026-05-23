c9c2ee9aa94067cd9bb5a47019a173e8789e8c03
Bindu <bindukrishnamurthyid@gmail.com>
2026-05-23 11:46:02 +0530

    message added

---
+++ src/main/java/com/example/CICD41/Calculator.java
@@ -0,0 +1,10 @@
+package com.example.CICD41;
+
+public class Calculator {
+	public static void main(String args[]) {
+int a= 10;
+int b= 20;
+int sum = a+b;
+System.out.println("Addition:"+sum);
+}
+}
